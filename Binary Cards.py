4a0d03f87381c637de1f7724017bad47a4700a61
rupin <rupin.chheda@gmail.com>
2016-05-12 15:54:56 +0530

    Initial Commit

---
+++ Binary Cards.py
@@ -0,0 +1,63 @@
+from PIL import Image
+import numpy as np
+from PIL import ImageFont
+from PIL import ImageDraw
+import math
+
+imgWidth=57
+imgHeight=88
+sizeMultiplier=3
+maxNumber=45
+maxExponent=math.ceil(math.log(maxNumber,2))
+columns=5
+fontSize=10
+maxNumbersPerCard=math.pow(2,maxExponent-1)
+maxRowCount=math.floor(maxNumbersPerCard/columns)
+#positionArray=np.ones((maxRowCount,columns))
+xPosM=imgWidth*sizeMultiplier/(columns)
+yPosM=xPosM*imgHeight/imgWidth
+
+
+if(maxNumber<30):
+    fontSize=20   
+elif(maxNumber<50):
+    fontSize=17
+elif(maxNumber<70):
+    fontSize=10
+
+def writeNumber(imgref, xpos, ypos, number):
+    
+    draw = ImageDraw.Draw(im)
+    font = ImageFont.truetype("arial.ttf", fontSize)
+    draw.text((xpos, ypos), number,(0,0,0),font=font)
+
+
+A=np.ones((imgHeight*sizeMultiplier,imgWidth*sizeMultiplier))
+A.fill(255)
+
+for cardNumber in range(0,maxExponent):
+    im = Image.fromarray(A)
+    if im.mode != 'RGB':
+        im = im.convert('RGB')
+    counter=0
+    rownum=0
+    colnum=-1
+    for number in range(1,maxNumber):
+        shiftedNumber=number>>cardNumber
+        if shiftedNumber%2==1:
+               
+               if(counter%columns==0):
+                   rownum=0
+                   colnum=colnum+1
+               else:
+                   rownum=rownum+1
+                   
+               writeNumber(im,(rownum*xPosM)+(xPosM/4),(colnum*yPosM)+xPosM/4,str(number))
+               counter=counter+1  
+    im.save(str(cardNumber)+".jpeg")
+
+
+
+
+
+
